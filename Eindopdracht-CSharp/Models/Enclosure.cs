e5fb972ce43c33771a4309b467d052c586566035
Jeroen <Jeroenpayens04@gmail.com>
2025-01-09 12:57:32 +0100

    Enclosure Constructor

--- Eindopdracht-CSharp/Models/Enclosure.cs
+++ Eindopdracht-CSharp/Models/Enclosure.cs
@@ -6,9 +6,23 @@ public class Enclosure
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public List<Animal> Animals { get; set; }
+    public List<Animal> Animals { get; set; } = new();
     public Climate Climate { get; set; }
     public HabitatType HabitatType { get; set; }
     public SecurityLevel SecurityLevel { get; set; }
     public double Size { get; set; } // square meters
+    
+    
+    public Enclosure(int id, string name, Climate climate, HabitatType habitatType, SecurityLevel securityLevel, double size)
+    {
+        Id = id;
+        Name = name;
+        Climate = climate;
+        HabitatType = habitatType;
+        SecurityLevel = securityLevel;
+        Size = size;
+    }
+    
+    
 }
+
